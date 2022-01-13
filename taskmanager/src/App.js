c6607ef65e23c66cfb465c97cd92abf378780da6
Harry Lam <harry33321@gmail.com>
2022-01-13 15:35:02 +0800

    created react

---
+++ taskmanager/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import TaskManager from './js/taskManager.js';
+
+function App() {
+  return (
+    <div>
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
