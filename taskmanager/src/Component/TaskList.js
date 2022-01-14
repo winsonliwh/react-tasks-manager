bcf1dae130f130c8f1a2b4dbb518100ec6e5a2bf
Harry Lam <harry33321@gmail.com>
2022-01-14 15:15:20 +0800

    commit
    Co-authored-by: Sing0424 <Sing0424@users.noreply.github.com>
    Co-authored-by: JackySze388 <JackySze388@users.noreply.github.com>

--- taskmanager/src/Component/TaskList.js
+++ taskmanager/src/Component/TaskList.js
@@ -1,8 +1,11 @@
-import React from 'react';
+// import React from 'react';
+import { useState } from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
 
-const task1 = [{
+
+const taskSample = [{
     id: 1,
     name: 'Take out the trash',
     description: 'Take out the trash to the front of the house',
@@ -18,53 +21,88 @@ const task1 = [{
     status: 'TODO'
 }]
 
-export default class TaskList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.tasks = [task1]
-        this.currentId = 0
-    }
+export default function TaskList() {
+    const [show, setShow] = useState(false);
 
-    addTask() {
-        this.tasks.push({task1})
-    }
+    const handleClose = (task) => setShow(false);
+    const handleShow = (task) => setShow(true);
 
-    render() {
-        return(
-            <div>
-                <Modal.Dialog>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Task Edit</Modal.Title>
-                    </Modal.Header>
+    // const addTask() {
+    //     this.tasks.push({ taskSample })
+    // }
 
-                    <Modal.Body>
-                        <div>
-                            Task ID: {this.tasks[0].id}
-                        </div>
-                        <div>
-                            Title: {this.tasks[0].name}
-                        </div>
-                        <div>
-                            Description: {this.tasks[0].description}
-                        </div>
-                        <div>
-                            Assigned To: {this.tasks[0].assignedTo}
-                        </div>
-                        <div>
-                            Due Date: {this.tasks[0].dueDate}
-                        </div>
-                        <div>
-                            Status: {this.tasks[0].status}
-                        </div>
-                    </Modal.Body>
+    // render() {
+    return (
+        <div>
+            {taskSample.map(task => (
+                <div key={task.id}>
+                    {console.log(task)}
+                    <div className="d-grid gap-2">
+                        <Button variant="secondary" size="lg" onClick={() => handleShow(task)}>
+                            {task.name}
+                        </Button>
+                    </div>
 
-                    <Modal.Footer>
-                        <Button variant="secondary">Close</Button>
-                        <Button variant="primary">Save changes</Button>
-                    </Modal.Footer>
-                </Modal.Dialog>
+                        <Modal key={task.id} show={show} onHide={() => setShow(false)}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>{task.name}</Modal.Title>
+                            </Modal.Header>
 
-            </div>
-        )
-    }
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Name</Form.Label>
+                                        <Form.Control value={task.name} placeholder="Name" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Description</Form.Label>
+                                        <Form.Control as="textarea" value={task.description} placeholder="Description" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Assigned To</Form.Label>
+                                        <Form.Control value={task.assignedTo} placeholder="Assigned To" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Due Date</Form.Label>
+                                        <Form.Control value={task.dueDate} type="date" placeholder="Due Date" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Status</Form.Label>
+                                        <Form.Select>
+                                            <option value="new">NEW</option>
+                                            <option value="done">DONE</option>
+                                        </Form.Select>
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="Task1" />
+                                    </Form.Group>
+                                </Form>
+                            </Modal.Body>
+
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+                                <Button variant="primary" onClick={handleClose} type="submit">
+                                    Save Changes
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    {/* <div show={show} onHide={handleClose}>
+                        Name: {task.name}<br />
+                        Description: {task.description}<br />
+                        Assigned To: {task.assignedTo}<br />
+                        Due Date: {task.dueDate}<br />
+                        Status: {task.status}<br /><br />
+                    </div> */}
+                </div>
+            ))}
+        </div>
+    )
+    // }
 }
