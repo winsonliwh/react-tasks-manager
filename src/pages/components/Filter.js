d7e95d0ba60b0b0f0e37dbffa29a08b34824f19f
HarryLam <harry33321@gmail.com>
2022-02-11 05:03:01 +0800

    add filter option

--- src/pages/components/Filter.js
+++ src/pages/components/Filter.js
@@ -9,6 +9,8 @@ import useLocalStorage from "use-local-storage";
 export default function Filter({ tasks, setfilterTasks }) {
 
     const [filterDone, setFilterDone] = useLocalStorage("filterDone", false);
+    const [sortByStartDate, setSortByStartDate] = useLocalStorage("sortByStartDate", false);
+    const [sortByDueDate, setSortByDueDate] = useLocalStorage("sortByDueDate", false);
     const [filterStatus, setFilterStatus] = useState({
         searchName: "",
         startDate: "",
@@ -63,7 +65,16 @@ export default function Filter({ tasks, setfilterTasks }) {
                 && (filterStatus.entertainmentStatus ? task.taskType === "Entertainment" : true)
                 && (filterDone ? !task.done : true)
         }));
-    }, [filterStatus, filterDone, tasks, setfilterTasks]);
+        setfilterTasks(prevTasks => {
+            if (sortByStartDate) {
+                return prevTasks.sort((a, b) => a.startDate - b.startDate)
+            } else if (sortByDueDate) {
+                return prevTasks.sort((a, b) => a.dueDate - b.dueDate)
+            } else {
+                return prevTasks
+            }
+        });
+    }, [filterStatus, filterDone, sortByStartDate, sortByDueDate, tasks, setfilterTasks]);
 
     const toggleFilter = () => {
         $(".filter").toggleClass("filterActive");
@@ -96,11 +107,6 @@ export default function Filter({ tasks, setfilterTasks }) {
 
                 <Form.Group>
                     <Form.Label className="filterTaskType"><p>Task Type</p></Form.Label>
-                    <Form.Check className="filterCheckBox" checked={filterDone} onChange={() => setFilterDone(!filterDone)}
-                        type="checkbox"
-                        label="Hide Done Tasks"
-                        id="doneStatus"
-                    />
                     <Form.Check className="filterCheckBox" checked={filterStatus.workStatus} onChange={handleCheckBoxStatus}
                         type="checkbox"
                         label="Work"
@@ -115,6 +121,21 @@ export default function Filter({ tasks, setfilterTasks }) {
                         type="checkbox"
                         label="Entertainment"
                         id="entertainmentStatus"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label className="filterTaskType"><p>Options</p></Form.Label>
+                    <Form.Check className="filterCheckBox" checked={filterDone} onChange={() => setFilterDone(!filterDone)}
+                        type="checkbox"
+                        label="Hide Done Tasks"
+                    />
+                    <Form.Check className="filterCheckBox" checked={sortByStartDate} onChange={() => setSortByStartDate(!sortByStartDate)}
+                        type="checkbox"
+                        label="Sort By Start Date"
+                    />
+                    <Form.Check className="filterCheckBox" checked={sortByDueDate} onChange={() => setSortByDueDate(!sortByDueDate)}
+                        type="checkbox"
+                        label="Sort By Due Date"
                     />
                 </Form.Group>
             </Form>
